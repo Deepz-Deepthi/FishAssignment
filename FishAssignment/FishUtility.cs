2ca87bed054484f74c3a111bbc72d1c62e149158
Deepz-Deepthi <deepuramesh2003@outlook.com>
2024-10-05 12:11:53 +0530

    Add project files.

---
+++ FishAssignment/FishUtility.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishAssignment
+{
+    public class FishUtility : Fish
+    {
+        public void AddFish(string species,double pricePerFish)
+        {
+            Species = species;
+            PricePerFish = pricePerFish;
+        }
+
+        public bool BuyFish()
+        {
+            if (Species == "Clownfish" || Species == "Goldfish")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public double CalculatePrice(int numberOfFishes)
+        {
+            double additionalCharges = 0;
+
+            if (Species == "Clownfish")
+            {
+                additionalCharges = 100;
+            }
+            else if (Species == "Goldfish")
+            {
+                additionalCharges = 150;
+            }
+            return (PricePerFish * numberOfFishes) + additionalCharges;
+        }
+    }
+}
---
+++ FishAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace FishAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FishUtility fishUtility = new FishUtility();
+
+            //prompt the user to enter the species to buy
+            Console.WriteLine("Enter the Species of the fish to buy : ");
+            string species = Console.ReadLine();
+            //prompt the user to enter the price per fish
+            Console.WriteLine("Enter the price per fish : ");
+            double pricePerFish = Convert.ToDouble(Console.ReadLine());
+             
+            fishUtility.AddFish(species, pricePerFish);
+
+            if (fishUtility.BuyFish())
+            {
+                Console.WriteLine("Enter the number of fishes you need to buy : ");
+                int numberOfFishes = Convert.ToInt32(Console.ReadLine());
+
+                double totalCost = fishUtility.CalculatePrice(numberOfFishes);
+                Console.WriteLine($"Total cost is : {totalCost}");
+            }
+            else
+            {
+                Console.WriteLine($"{species} species not valid");
+            }
+
+        }
+    }
+}
